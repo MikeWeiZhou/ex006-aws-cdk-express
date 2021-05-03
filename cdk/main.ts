85b3fe44698f35e3c717ee5c2c4c4d8d17e41a11
Wei Yi Zhou <mikeweizhou@gmail.com>
2021-05-03 00:54:28 -0700

    refactor(cdk): move all cdk files into cdk folder

--- cdk/main.ts
+++ cdk/main.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkStack } from './cdk-stack';
+import { CdkStack } from './stacks/cdk-stack';
 
 const app = new cdk.App();
 new CdkStack(app, 'CdkStack', {
