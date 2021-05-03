6affd90543d6e28191e1f8a980338b59e69a08ba
Wei Yi Zhou <mikeweizhou@gmail.com>
2021-05-03 00:19:49 -0700

    refactor(cdk): clean up generated codebase
    - moved files from /bin and /lib into /cdk
    - removed jest package and .npmignore

--- cdk/main.ts
+++ cdk/main.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { RefCdkExpressStack } from '../lib/ref-cdk-express-stack';
+import { CdkStack } from './cdk-stack';
 
 const app = new cdk.App();
-new RefCdkExpressStack(app, 'RefCdkExpressStack', {
+new CdkStack(app, 'CdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
