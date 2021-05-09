9575b2796fe264979e1683ca8bc2757e35ce9cac
Wei Yi Zhou <mikeweizhou@gmail.com>
2021-05-08 18:08:40 -0700

    fix(db): set charset and collation to utf8
    Both deployments and local database now share the same character set and
    collation: utf8 and utf8_col.
    
    MySQL 5.7 defaults to latin1, but even they recommend setting to utf8:
    https://dev.mysql.com/doc/mysql-installation-excerpt/5.7/en/docker-mysql-more-topics.html

--- cdk/stacks/db-nested-stack.ts
+++ cdk/stacks/db-nested-stack.ts
@@ -39,11 +39,33 @@ export class DbNestedStack extends cdk.NestedStack {
   constructor(scope: cdk.Construct, id: string, props: DbNestedStackProps) {
     super(scope, id, props);
 
+    // Database engine to use
+    const engine = rds.DatabaseClusterEngine.auroraMysql({
+      version: rds.AuroraMysqlEngineVersion.VER_5_7_12,
+    });
+
+    // Parameters sent to database cluster and instances
+    // https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/AuroraMySQL.Reference.html#AuroraMySQL.Reference.ParameterGroups
+    const parameterGroup = new rds.ParameterGroup(this, 'DbParameterGroup', {
+      engine,
+      parameters: {
+        /* Default MySQL 5.7 character set and collation is latin1, but utf8 is recommended by
+         * MySQL themself.
+         *
+         * https://dev.mysql.com/doc/mysql-installation-excerpt/5.7/en/docker-mysql-more-topics.html
+         * https://stackoverflow.com/questions/2708958/differences-between-utf8-and-latin1
+         * https://stackoverflow.com/questions/30074492/what-is-the-difference-between-utf8mb4-and-utf8-charsets-in-mysql
+         */
+        character_set_server: 'utf8mb4',
+        collation_server: 'utf8mb4_unicode_ci',
+      },
+    });
+
+    // Create new database cluster
     this.db = new rds.ServerlessCluster(this, 'ServerlessDbCluster', {
       vpc: props.vpcNestedStack.vpc,
-      engine: rds.DatabaseClusterEngine.auroraMysql({
-        version: rds.AuroraMysqlEngineVersion.VER_5_7_12,
-      }),
+      engine,
+      parameterGroup,
       credentials: rds.Credentials.fromPassword(
         props.adminUser.toString(),
         props.adminPassword,
